b198d7be472e2d91acbfc44d7b5dee415efecc6c
ipsfenny <43324244+ipsfenny@users.noreply.github.com>
2023-11-13 19:41:42 +0100

    random commit

--- main.c
+++ main.c
@@ -3,10 +3,13 @@
 int main (void){
     printf("Dieses Programm ist toll \n");
     printf("zweite Ausgabe \n");
+    ausgabe();
     return 0;
 }
-int ausgabe (void){
+int ausgabe (int a){
     printf("Ausführen der Funktion Ausgabe\n");
+    return 3;
 }
+
 
 //einfaches Testprogramm um die Funktionalität von Git und VC Code zu überprüfen
