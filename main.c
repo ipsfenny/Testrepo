5d5117628e5db0bebb8ba6253d8f79af31e2d782
ipsfenny <43324244+ipsfenny@users.noreply.github.com>
2023-11-13 18:29:31 +0100

    erster commit
    Erster commit um zu überprüfen ob Git auf dem Gerät funktioniert.

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main (void){
+    printf("Dieses Programm ist toll \n");
+    return 0;
+}
+
+//einfaches Testprogramm um die Funktionalität von Git und VC Code zu überprüfen
